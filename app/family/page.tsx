8e0ee388b08c1c1d5de6e3401937615b3f1e0fbe
terry-richardson-photography <9dftsfnc5r@privaterelay.appleid.com>
2026-02-23 19:05:57 +1030

    Fix missing useState import

--- app/family/page.tsx
+++ app/family/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { sanityClient } from "../../lib/sanity";
 import { urlFor } from "../../lib/image";
