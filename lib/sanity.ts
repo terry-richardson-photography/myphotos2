a532a31d71cec4780ee54a7fc459034409bba83d
terry-richardson-photography <9dftsfnc5r@privaterelay.appleid.com>
2026-02-24 07:28:53 +1030

    Fix projectId string format

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity'
 
 export const sanityClient = createClient({
-  projectId: 3zdn0cy5,
+  projectId: "3zdn0cy5",
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: true,
