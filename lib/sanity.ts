68af05e5786e054be26e24b25d9db010bfd0a081
terry-richardson-photography <9dftsfnc5r@privaterelay.appleid.com>
2026-02-23 22:13:30 +1030

    Temporary hardcoded Sanity config

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,8 +1,8 @@
-import { createClient } from "next-sanity";
+import { createClient } from 'next-sanity'
 
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: 3zdn0cy5,
+  dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: true,
-});
+})
